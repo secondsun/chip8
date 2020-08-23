0ba8b3540f8aca30d769046ba6e181d954acce18
Summers Pittman <secondsun@gmail.com>
2020-08-23 14:34:10 -0400

    Initial failing unit test

--- src/test/java/net/saga/console/chip8/test/E09JITTest.java
+++ src/test/java/net/saga/console/chip8/test/E09JITTest.java
@@ -1,16 +1,27 @@
 package net.saga.console.chip8.test;
 
 
+import net.saga.console.chip8.Chip8;
+import net.saga.console.chip8.util.Chip8Utils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class E09JITTest {
 
+    private Chip8 chip8;
+
+    /**
+     * This test takes our example program from E07GraphicsTest and jits it. This program is an infinite loop.
+     * @throws IOException
+     */
     @Test
-    @DisplayName("JIT a simple program")
-    public void jitProgram() {
+    @DisplayName("JIT an infinite loop")
+    public void jitProgram() throws IOException {
+        this.chip8 = Chip8Utils.createFromRom(getClass().getResource("/E07GraphicsRom.ch8"));
         fail("not implemented");
     }
 
